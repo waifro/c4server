5e49e44f4210ff88e8c2df43f6d2d7fbc11cb443
waifro <waifro.bio@gmail.com>
2022-09-20 17:18:00 +0200

    minor fix

--- source/main.c
+++ source/main.c
@@ -199,6 +199,7 @@ int main(int argc, char *argv[]) {
 
 					client_disconnect(glo_client_list, i, glo_lobby, &addr, &connected, MAX_CLIENTS);
 					printf("glo_client_list after died: %d\n", glo_client_list[i]);
+					getchar();
 					
                 } else if (cl_status_LOBBY(result) == 0) {
 
